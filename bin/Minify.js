ec1ed0c2089fafe6258557fb54090a0e7a3d7f62
obara.takao <obara.takao@dena.jp>
2014-04-22 22:22:56 +0900

    auto save build settings

--- bin/Minify.js
+++ bin/Minify.js
@@ -16,11 +16,12 @@ var _USAGE = _multiline(function() {/*
                        [--simple]
                        [--strict]
                        [--pretty]
-                       [--save json-file]
+                       [--save build.json]
                        [--option "compile option"]
                        [--extern extern-file]
                        [--output output-file]
                        [--module]
+                       [--release]
                        [input-files [input-files...]]
 
     See:
@@ -37,34 +38,41 @@ var _CONSOLE_COLOR = {
 var fs      = require("fs");
 var Minify  = require("../lib/Minify");
 var argv    = process.argv.slice(2);
-var io      = _loadCurrentDirectoryPackageJSON();
-var options = _parseCommandLineOptions({
-        brew:       false,      // Boolean: use brew installed closure-compiler
-        help:       false,      // Boolean: true is show help.
-        keep:       false,      // Boolean: keep tmp file.
-        save:       "",         // PathString: save to path.
-        nowrap:     false,      // Boolean: false is wrap WebModule idiom.
-        header:     "",         // PathString: header file
-        footer:     "",         // PathString: footer file
-        strict:     false,      // Boolean: true is add 'use strict'
-        pretty:     false,      // Boolean: true is pretty print
-        inputs:     io.inputs,  // PathStringArray: ["input-file-name", ...]
-        output:     io.output,  // PathString: "output-file-name"
-        option:     [],         // OptionStringArray: ["language_in ECMASCRIPT5_STRICT", ...];
-        compile:    true,       // Boolean: true is compile
-        externs:    [],         // ExternFilePathArray: ["externs-file-name", ...]
-        modules:    [],         // ModuleNameStringArray: [moduel, ...]
-        verbose:    false,      // Boolean: true is verbose mode.
-        workDir:    "",         // PathString: work dir.
-        advanced:   true,       // Boolean: true is ADVANCED_OPTIMIZATIONS MODE
-        preprocess: ["assert"]  // LabelStringArray: ["assert", "debug", ...]
-    });
+var package = _loadCurrentDirectoryPackageJSON();
+var options = {
+        name:       package.name,   // Object: { git:String, npm:String }. github repository name, npm package name.
+        brew:       false,          // Boolean: use brew installed closure-compiler.
+        help:       false,          // Boolean: true is show help.
+        keep:       false,          // Boolean: keep tmp file.
+        save:       "build.json",   // PathString: save to path.
+        nowrap:     false,          // Boolean: false is wrap WebModule idiom.
+        header:     "",             // PathString: header file.
+        footer:     "",             // PathString: footer file.
+        strict:     false,          // Boolean: true is add 'use strict'.
+        pretty:     false,          // Boolean: true is pretty print.
+        files:      package.files,  // PathStringArray: ["input-file-name", ...]
+        moduleFiles:[],             // PathStringArray: ["input-file-name", ...]
+        output:     package.output, // PathString: "output-file-name"
+        option:     [],             // OptionStringArray: ["language_in ECMASCRIPT5_STRICT", ...];
+        compile:    true,           // Boolean: true is compile.
+        release:    false,          // Boolean: true is release build, use moduleFiles.
+        externs:    [],             // ExternFilePathArray: ["externs-file-name", ...]
+        modules:    [],             // ModuleNameStringArray: [moduel, ...]
+        verbose:    false,          // Boolean: true is verbose mode.
+        workDir:    "",             // PathString: work dir.
+        advanced:   true,           // Boolean: true is ADVANCED_OPTIMIZATIONS MODE.
+        preprocess: ["assert"]      // LabelStringArray: ["assert", "debug", ...]
+    };
+
+_loadModule(options, "./", "package.json");
+
+options = _parseCommandLineOptions(options);
 
 if (options.help) {
     console.log(_CONSOLE_COLOR.YELLOW + _USAGE + _CONSOLE_COLOR.CLEAR);
     return;
 }
-if (!options.inputs.length) {
+if (!options.files.length) {
     console.log(_CONSOLE_COLOR.RED + "Input files are empty." + _CONSOLE_COLOR.CLEAR);
     return;
 }
@@ -77,22 +85,8 @@ if (!options.workDir.length) {
     return;
 }
 
-if (options.verbose || options.save) {
-    var packageJSON = JSON.parse( fs.readFileSync("./package.json") );
-    var object = {
-        name:    packageJSON.name,
-        modules: options.modules,
-        inputs:  options.inputs
-    };
-    if (options.verbose) {
-        console.log(_CONSOLE_COLOR.GREEN + JSON.stringify(object, null, 2) + _CONSOLE_COLOR.CLEAR);
-    }
-    if (options.save) {
-        fs.writeFileSync(options.save, JSON.stringify(object, null, 2));
-    }
-}
-
-Minify(options.inputs, {
+Minify(options.release ? options.moduleFiles
+                       : options.files, {
     "brew":         options.brew,
     "keep":         options.keep,
     "nowrap":       options.nowrap,
@@ -110,26 +104,51 @@ Minify(options.inputs, {
 }, function(err,  // @arg Error:
             js) { // @arg String: minified JavaScript Expression string.
     fs.writeFileSync(options.output, js);
+
+    if (options.save) {
+        _saveBuildSettings(options);
+    }
 });
 
 function _loadCurrentDirectoryPackageJSON() {
     var path   = "./package.json";
     var json   = fs.existsSync(path) ? JSON.parse(fs.readFileSync(path, "utf8")) : {};
+    var npm    = json["name"] || "";
+    var git    =(json["url"] || "").split("/").pop();
     var build  = json["x-build"] || json["build"] || {};
-    var inputs = build.inputs || [];
+    var files  = build.files  || build.inputs || [];        // build.files is deprecated.
     var output = build.output || "";
 
-    return { inputs: inputs, output: output };
+    return {
+        name:   { git: git, npm: npm },
+        files:  files,
+        output: output
+    };
+}
+
+function _saveBuildSettings(options) {
+    var saveSettingsObject = {
+            solo: {
+                name:   options.name,
+                files:  options.files,
+                modules:[]
+            },
+            release: {
+                name:   options.name,
+                files:  options.moduleFiles,
+                modules:options.modules
+            }
+        };
+
+    if (options.verbose) {
+        console.log(_CONSOLE_COLOR.GREEN +
+                    "\nSave Build settings:  \n" + options.save + ": " +
+                    JSON.stringify(saveSettingsObject, null, 2) + _CONSOLE_COLOR.CLEAR);
+    }
+    fs.writeFileSync(options.save, JSON.stringify(saveSettingsObject, null, 2) + "\n");
 }
 
 function _parseCommandLineOptions(options) {
-    for (var i = 0, iz = argv.length; i < iz; ++i) {
-        switch (argv[i]) {
-        case "--module":    options.inputs = []; // clear
-                            _loadModule(options, "./", "package.json");
-        }
-    }
-
     for (var i = 0, iz = argv.length; i < iz; ++i) {
         switch (argv[i]) {
         case "-h":
@@ -149,14 +168,19 @@ function _parseCommandLineOptions(options) {
         case "--output":    options.output = argv[++i]; break;
         case "--extern":    options.externs.push( argv[++i] ); break;
         case "--option":    options.option.push( argv[++i] ); break;
-        case "--module":    break;
+        case "--module":
+        case "--relase":    options.release = true; break;
         default:
             if (argv[i][0] === "@") {
                 options.preprocess.push(argv[i].slice(1));
             } else {
-                if (options.inputs.indexOf("./" + argv[i]) < 0 &&
-                    options.inputs.indexOf(       argv[i]) < 0) { // avoid duplicate
-                    options.inputs.push(argv[i]);
+                if (options.files.indexOf("./" + argv[i]) < 0 &&
+                    options.files.indexOf(       argv[i]) < 0) { // avoid duplicate
+                    options.files.push(argv[i]);
+                }
+                if (options.moduleFiles.indexOf("./" + argv[i]) < 0 &&
+                    options.moduleFiles.indexOf(       argv[i]) < 0) { // avoid duplicate
+                    options.moduleFiles.push(argv[i]);
                 }
             }
         }
@@ -173,7 +197,7 @@ function _parseCommandLineOptions(options) {
     return options;
 }
 
-function _loadModule(options, // @arg Object: { inputs, modules }
+function _loadModule(options, // @arg Object: { files, modules }
                      dir,     // @arg String: "./"
                      file) {  // @arg String: "package.json"
     var json = JSON.parse( fs.readFileSync(dir + file) );
@@ -194,10 +218,10 @@ function _loadModule(options, // @arg Object: { inputs, modules }
     var build = json["x-build"] || json["build"];
 
     if (build) {
-        if (Array.isArray(build.inputs)) {
-            build.inputs.forEach(function(file) {
-                if (options.inputs.indexOf(file) < 0) { // avoid duplicate
-                    options.inputs.push(dir + file);
+        if (Array.isArray(build.files)) {
+            build.files.forEach(function(file) {
+                if (options.moduleFiles.indexOf(file) < 0) { // avoid duplicate
+                    options.moduleFiles.push(dir + file);
                 }
             });
         }
