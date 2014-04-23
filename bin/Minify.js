81cae4da573ba9eee464f6bba51bc16d7b50d402
obara.takao <obara.takao@dena.jp>
2014-04-23 22:02:10 +0900

    follow NodeModule improvement, remove --save option, not create build.json

--- bin/Minify.js
+++ bin/Minify.js
@@ -16,7 +16,6 @@ var _USAGE = _multiline(function() {/*
                        [--simple]
                        [--strict]
                        [--pretty]
-                       [--save build.json]
                        [--option "compile option"]
                        [--extern extern-file]
                        [--output output-file]
@@ -38,13 +37,13 @@ var _CONSOLE_COLOR = {
 var fs      = require("fs");
 var Minify  = require("../lib/Minify");
 var argv    = process.argv.slice(2);
+var NodeModule = require("uupaa.nodemodule.js");
 var package = _loadCurrentDirectoryPackageJSON();
 var options = {
         name:       package.name,   // Object: { git:String, npm:String }. github repository name, npm package name.
         brew:       false,          // Boolean: use brew installed closure-compiler.
         help:       false,          // Boolean: true is show help.
         keep:       false,          // Boolean: keep tmp file.
-        save:       "build.json",   // PathString: save to path.
         nowrap:     false,          // Boolean: false is wrap WebModule idiom.
         header:     "",             // PathString: header file.
         footer:     "",             // PathString: footer file.
@@ -52,11 +51,6 @@ var options = {
         pretty:     false,          // Boolean: true is pretty print.
         files:      package.files,  // PathStringArray: package.json x-build.files. ["input-file-name", ...]
         target:     package.target, // StringArray: build target. ["Browser", "Worker", "Node"]
-        moduleTargets: {},          // Object: { moduleName: ["Browser", "Worker", "Node"], ... }
-        dependenciesFiles: [],      // PathStringArray: dependencies files. ["input-file-name", ...]
-        dependenciesModules: [],    // ModuleNameStringArray: [module, ...]
-        devDependenciesFiles: [],   // PathStringArray: devDependencies files. ["input-file-name", ...]
-        devDependenciesModules: [], // ModuleNameStringArray: [module, ...]
         output:     package.output, // PathString: "output-file-name"
         option:     [],             // OptionStringArray: ["language_in ECMASCRIPT5_STRICT", ...];
         compile:    true,           // Boolean: true is compile.
@@ -68,9 +62,6 @@ var options = {
         preprocess: ["assert"]      // LabelStringArray: ["assert", "debug", ...]
     };
 
-options.moduleTargets[package.name.npm] = package.target;
-_loadDependenciesModule(options, "", "package.json");
-_loadDevDependenciesModule(options, "", "package.json");
 
 options = _parseCommandLineOptions(options);
 
@@ -94,7 +85,9 @@ if (!options.workDir.length) {
 var files = options.files;
 
 if (options.release) {
-    files = [].concat(options.dependenciesFiles, options.files);
+    var nodeData = NodeModule.load({ develop: true });
+
+    files = [].concat(nodeData.dependenciesFiles, options.files);
 }
 
 Minify(files, {
@@ -115,10 +108,6 @@ Minify(files, {
 }, function(err,  // @arg Error:
             js) { // @arg String: minified JavaScript Expression string.
     fs.writeFileSync(options.output, js);
-
-    if (options.save) {
-        _saveBuildSettings(options);
-    }
 });
 
 function _loadCurrentDirectoryPackageJSON() {
@@ -139,29 +128,6 @@ function _loadCurrentDirectoryPackageJSON() {
     };
 }
 
-function _saveBuildSettings(options) {
-    var saveSettingsObject = {
-            name: options.name,
-            build: {
-                files:  options.files,
-                output: options.output,
-                target: options.target
-            },
-            moduleTargets: options.moduleTargets,
-            dependenciesFiles: options.dependenciesFiles,
-            dependenciesModules: options.dependenciesModules,
-            devDependenciesFiles: options.devDependenciesFiles,
-            devDependenciesModules: options.devDependenciesModules
-        };
-
-    if (options.verbose) {
-        console.log(_CONSOLE_COLOR.GREEN +
-                    "\nSave Build settings:  \n" + options.save + ": " +
-                    JSON.stringify(saveSettingsObject, null, 2) + _CONSOLE_COLOR.CLEAR);
-    }
-    fs.writeFileSync(options.save, JSON.stringify(saveSettingsObject, null, 2) + "\n");
-}
-
 function _parseCommandLineOptions(options) {
     for (var i = 0, iz = argv.length; i < iz; ++i) {
         switch (argv[i]) {
@@ -170,7 +136,6 @@ function _parseCommandLineOptions(options) {
         case "-v":
         case "--brew":      options.brew = true; break;
         case "--verbose":   options.verbose = true; break;
-        case "--save":      options.save = argv[++i]; break;
         case "--nowrap":    options.nowrap = true; break;
         case "--nocompile": options.compile = false; break;
         case "--header":    options.header = fs.readFileSync(argv[++i], "utf8"); break;
@@ -204,88 +169,6 @@ function _parseCommandLineOptions(options) {
         }
     }
     return options;
-}
-
-function _loadDependenciesModule(options,  // @arg Object: { dependenciesFiles, dependenciesModules }
-                                 dir,      // @arg String: ""
-                                 file) {   // @arg String: "package.json"
-    var json = JSON.parse( fs.readFileSync(dir + file) );
-
-    if (json.dependencies) {
-        Object.keys(json.dependencies).forEach(function(moduleName) {
-
-            if (options.dependenciesModules.indexOf(moduleName) < 0) { // avoid duplicate
-                var path = dir + "node_modules/" + moduleName + "/" + file;
-
-                if (fs.existsSync(path)) {
-                    options.dependenciesModules.push(moduleName);
-                    _loadDependenciesModule(options, dir + "node_modules/" + moduleName + "/", file);
-                }
-            }
-        });
-    }
-    if (dir) {
-        var build = json["x-build"] || json["build"];
-
-        if (build) {
-            var files = build.files || build.inputs || null; // build.inputs is deprecated.
-
-            if (Array.isArray(files)) {
-                files.forEach(function(file) {
-                    if (options.dependenciesFiles.indexOf(file) < 0) { // avoid duplicate
-                        options.dependenciesFiles.push(dir + file);
-                    }
-                });
-            }
-            if (!build.target) {
-                if (options.verbose) {
-                    console.log(_CONSOLE_COLOR.YELLOW + "build.target undefined in " + json.name + _CONSOLE_COLOR.CLEAR);
-                }
-            }
-            options.moduleTargets[json.name] = build.target;
-        }
-    }
-}
-
-function _loadDevDependenciesModule(options,  // @arg Object: { devDependenciesFiles, devDependenciesModules }
-                                    dir,      // @arg String: ""
-                                    file) {   // @arg String: "package.json"
-    var json = JSON.parse( fs.readFileSync(dir + file) );
-
-    if (json.devDependencies) {
-        Object.keys(json.devDependencies).forEach(function(moduleName) {
-
-            if (options.devDependenciesModules.indexOf(moduleName) < 0) { // avoid duplicate
-                var path = dir + "node_modules/" + moduleName + "/" + file;
-
-                if (fs.existsSync(path)) {
-                    options.devDependenciesModules.push(moduleName);
-                    _loadDevDependenciesModule(options, dir + "node_modules/" + moduleName + "/", file);
-                }
-            }
-        });
-    }
-    if (dir) {
-        var build = json["x-build"] || json["build"];
-
-        if (build) {
-            var files = build.files || build.inputs || null; // build.inputs is deprecated.
-
-            if (Array.isArray(files)) {
-                files.forEach(function(file) {
-                    if (options.devDependenciesFiles.indexOf(file) < 0) { // avoid duplicate
-                        options.devDependenciesFiles.push(dir + file);
-                    }
-                });
-            }
-            if (!build.target) {
-                if (options.verbose) {
-                    console.log(_CONSOLE_COLOR.YELLOW + "build.target undefined in " + json.name + _CONSOLE_COLOR.CLEAR);
-                }
-            }
-            options.moduleTargets[json.name] = build.target;
-        }
-    }
 }
 
 function _multiline(fn) { // @arg Function:
